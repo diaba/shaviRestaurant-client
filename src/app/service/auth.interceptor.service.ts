e599d302c2730f9845182ddc65eaa9254649ed96
diaba <hwar@cincinnatistate.edu>
2022-01-31 19:35:13 -0500

    Completed authentification interception

---
+++ src/app/service/auth.interceptor.service.ts
@@ -0,0 +1,28 @@
+// Credit : https://github.com/bezkoder/angular-12-jwt-authentication
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+import { Observable } from 'rxjs';
+import { TokenStorageService } from './token-storage.service';
+
+const TOKEN_HEADER_KEY = 'Authorization';       // for Spring Boot back-end
+
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+  constructor(private token: TokenStorageService) { }
+
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    let authReq = req;
+    const token = this.token.getToken();
+    if (token != null) {
+      // for Spring Boot back-end
+       authReq = req.clone({ headers: req.headers.set(TOKEN_HEADER_KEY, 'Bearer ' + token) });
+
+    }
+    return next.handle(authReq);
+  }
+}
+
+export const authInterceptorProviders = [
+  { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true }
+];
